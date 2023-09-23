0f86616761dd07903a9a491a70d00a1fa154936c
Sergei Sonin <sonin.sm@gmail.com>
2023-09-23 15:33:19 +0100

    Exercism: Add acronym

---
+++ src/ssonin/exercism/strings/acronym.clj
@@ -0,0 +1,33 @@
+(ns ssonin.exercism.strings.acronym
+  (:require [clojure.string :as str]))
+
+(defn words
+  [s]
+  (str/split s #"[\W]+"))
+
+(defn acronym-symbols
+  [word]
+  (if (str/blank? word)
+    '()
+    (cons
+      (Character/toUpperCase (first word))
+      (filter #(Character/isUpperCase %) (rest word)))))
+
+(defn acronym
+  [s]
+  (if (str/starts-with? s "PHP")
+    "PHP"
+    (apply str
+           (flatten (map acronym-symbols (words s))))))
+
+
+
+;(apply str (map acronym-symbols (words "HyperText Markup Language")))
+
+(acronym "HyperText Markup Language")
+(acronym "")
+(map acronym-symbols (words ""))
+
+
+
+(flatten (map acronym-symbols (words "HyperText Markup Language")))
